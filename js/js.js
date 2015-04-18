6b7483e2a99840671eeb6b481ac294e50efb702a
Renato Cardoso <re2005@gmail.com>
2015-04-18 17:26:14 +0200

    Rooms total cost ready

--- js/js.js
+++ js/js.js
@@ -8,7 +8,7 @@
 	var totalPhotos;
 
 	//Create SlideShow
-	createSlider =	function() {
+	createSlider =  function() {
 		var  slider = "<div id=photoSlider><div class=wrap/><div class=nav><span class=bar/><span id=prev/><span id=next/><span class=close/>";
 		$( 'body' ).prepend(slider);
 		var index = $( '.one_photo' ).size();
@@ -46,7 +46,7 @@
 	  if (e.keyCode == 27) { closePhotos() }   // esc
 	  if (e.keyCode == 39) { navSlider("next"); }   // right
 	  if (e.keyCode == 37) { navSlider(); }   // left
-	});	
+	}); 
 
 
 
@@ -67,7 +67,7 @@
 			} else {
 				currentPhoto = currentPhoto -1 ;
 				openPhoto(currentPhoto,"true");
-			}			
+			}           
 		}
 	}
 
@@ -89,7 +89,7 @@
 				$( '#photoSlider .wrap' ).css( "top", "5%" );
 				$( '#photoSlider h2' ).html(alt);
 				$('.nav').delay(500).css( "top", "5%" );
-			});			
+			});         
 		}
 	}
 
@@ -118,37 +118,73 @@
 
 	slideShow = function() {
 		slideTimer = window.setTimeout(
-		function() {
-			$( "#next" ).trigger( "click" );
-			slideShow();
-		}, 3000);
+			function() {
+				$( "#next" ).trigger( "click" );
+				slideShow();
+			}, 3000);
 	}
 
 
 
-    $.ajax({
-      dataType: "json",
-      url: "js/data.json",
-      jsonp: "$callback",
-      success: starPage
-    });
-    
 
-    
-    function starPage( data ) {
-      $("#photoTemplate").tmpl(data).appendTo(".photos");
-      $("#headerTemplate").tmpl(data).appendTo("header");
-      $("#descriptionTemplate").tmpl(data).appendTo(".description");
-      $("#facilitiesTemplate").tmpl(data).appendTo("#facilities");
-    }
+	$.ajax({
+		dataType: "json",
+		url: "js/data.json",
+		jsonp: "$callback",
+		success: starPage
+	});
+	function starPage( data ) {
+		$("#photoTemplate").tmpl(data).appendTo(".photos");
+		$("#headerTemplate").tmpl(data).appendTo("header");
+		$("#descriptionTemplate").tmpl(data).appendTo(".description");
+		$("#facilitiesTemplate").tmpl(data).appendTo("#facilities");
+		$("#roomsTemplate").tmpl(data).appendTo("tbody");
+	}
+
+
+
+	$(document).on('change', '.room_quantity select', function() {
+		var cost = $(this).attr('data-price');
+		totalCost();
+	})
+
+
+	function totalCost(){
+		var t_sum = 0;
+		$( "select" ).each(function( index ) {
+			var qnt = $(this).val()
+			var price = $(this).attr( 'data-price' );
+			var sum = parseFloat(price.replace(',','.')) * parseFloat(qnt);
+	        t_sum += sum;
+	        total_sum = t_sum.toFixed(2);
+			$("#total_price").text(total_sum);
+		});
+
+		$(".rooms h3").addClass("alert_1").delay(1000).queue(function(next){
+			$(this).removeClass();
+			next();
+		});
+	}
+
+
+
+
+
+
+
+	// Google Maps
+	function initialize() {
+		var mapCanvas = document.getElementById('map-canvas');
+		var mapOptions = {
+			center: new google.maps.LatLng(48.8421607,2, -2.3994408,13),
+			zoom: 12,
+			mapTypeId: google.maps.MapTypeId.ROADMAP
+		}
+		var map = new google.maps.Map(mapCanvas, mapOptions)
+	}
+	google.maps.event.addDomListener(window, 'load', initialize);
 
 
 })();
-
-
-
-
-
-
 
 
