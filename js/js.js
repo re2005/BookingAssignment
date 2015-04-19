dc7dad38de0d538960f24dd356aa542d50d82457
Renato Cardoso <re2005@gmail.com>
2015-04-19 23:25:23 +0200

    Review from Json, js organisation

--- js/js.js
+++ js/js.js
@@ -4,34 +4,11 @@
 
 (function (){
 
-	var currentPhoto;
-	var totalPhotos;
 
-	//Create SlideShow
-	createSlider =  function() {
-		var  slider = "<div id=photoSlider><div class=wrap/><div class=nav><span class=bar/><span id=prev/><span id=next/><span class=close/>";
-		$( 'body' ).prepend(slider);
-		var index = $( '.one_photo' ).size();
-		totalPhotos = index;
-	}
-	setTimeout(function(){ createSlider(); }, 100);
-
-
-	// Navigation
-
-	var f_sl = 1;
-	var f_nm = 1;
-	$(document).on('click', '.room_occupancy, .room_price', function(e) {
-		e.preventDefault();
-		f_nm *= -1;
-		var n = $(this).prevAll().length;
-		sortTable(f_nm,n);		
-
-	});
-
-
-
-
+	////////////////////////////////////////////////////////////
+	// PAGE NAVIGATION
+	///////////////////////////////////////////////////////////
+	
 	$(document).on('click', '.one_photo', function(e) {
 		e.preventDefault();
 		slideShow()
@@ -56,12 +33,59 @@
 	});
 
 	$(document).keyup(function(e) {
-	  if (e.keyCode == 27) { closePhotos() }   // esc
-	  if (e.keyCode == 39) { navSlider("next"); }   // right
-	  if (e.keyCode == 37) { navSlider(); }   // left
+		if (e.keyCode == 27) { closePhotos() }   // esc
+		if (e.keyCode == 39) { navSlider("next"); }   // right
+		if (e.keyCode == 37) { navSlider(); }   // left
 	}); 
 
 
+
+
+
+	////////////////////////////////////////////////////////////
+	// CREATE SLIDESHOW ELEMENTS and FUNCTIONS
+	///////////////////////////////////////////////////////////
+
+	var currentPhoto;
+	var totalPhotos;
+	createSlider =  function() {
+		var  slider = "<div id=photoSlider><div class=wrap/><div class=nav><span class=bar/><span id=prev/><span id=next/><span class=close/>";
+		$( 'body' ).prepend(slider);
+		var index = $( '.one_photo' ).size();
+		totalPhotos = index;
+	}
+	setTimeout(function(){ createSlider(); }, 100);
+
+	openPhoto = function(index,nav){
+		if ( index ) {
+			var url = getPhotoData(index).url;
+			var alt = getPhotoData(index).alt;
+			if ( nav ) {
+				$( '#photoSlider h2' ).html(alt);
+				$( "#photoSlider .wrap" ).prepend('<img src='+url+'>');
+				$( "#photoSlider .wrap img" ).last().fadeOut(200, function() {
+					$(this).remove();
+				});;
+			}
+			else {
+				$( "#photoSlider .wrap" ).prepend('<img src='+url+'><h2>');
+				$( '#photoSlider' ).hide().fadeIn(200, function() {
+					$( '#photoSlider .wrap' ).css( "top", "5%" );
+					$( '#photoSlider h2' ).html(alt);
+					$('.nav').delay(500).css( "top", "5%" );
+				});
+			}
+		}
+	}
+
+	closePhotos = function(){
+		$( '#photoSlider' ).fadeOut(200, function() {
+			$( this ).hide();
+			$( '#photoSlider .wrap' ).css( "top", "-100%" ).empty();
+			$('.nav').delay(500).css( "top", "-100%" );
+			clearTimeout( slideTimer );
+		});
+	}
 
 	navSlider = function(nav) {
 		if (nav == "next") {
@@ -84,40 +108,6 @@
 		}
 	}
 
-
-
-	openPhoto = function(index,nav){
-		var url = getPhotoData(index).url;
-		var alt = getPhotoData(index).alt;
-		if ( nav ) {
-			$( '#photoSlider h2' ).html(alt);
-			$( "#photoSlider .wrap" ).prepend('<img src='+url+'>');
-			$( "#photoSlider .wrap img" ).last().fadeOut(200, function() {
-				$(this).remove();
-			});;
-		}
-		else {
-			$( "#photoSlider .wrap" ).prepend('<img src='+url+'><h2>');
-			$( '#photoSlider' ).hide().fadeIn(200, function() {
-				$( '#photoSlider .wrap' ).css( "top", "5%" );
-				$( '#photoSlider h2' ).html(alt);
-				$('.nav').delay(500).css( "top", "5%" );
-			});         
-		}
-	}
-
-
-	closePhotos = function(){
-		$( '#photoSlider' ).fadeOut(200, function() {
-			console.log("GA Event: User close photoSlider");
-			$( this ).hide();
-			$( '#photoSlider .wrap' ).css( "top", "-100%" ).empty();
-			$('.nav').delay(500).css( "top", "-100%" );
-			clearTimeout( slideTimer );
-		});
-	}
-
-
 	getPhotoData = function(index){
 		var photos = [];
 		$( ".photos ul li a" ).each(function( index ) {
@@ -127,7 +117,6 @@
 		});
 		return photos[index];
 	}
-
 
 	slideShow = function() {
 		slideTimer = window.setTimeout(
@@ -139,22 +128,33 @@
 
 
 
+	////////////////////////////////////////////////////////////
+	// LOAD DATA and BUILD PAGE
+	///////////////////////////////////////////////////////////
 
 	$.ajax({
 		dataType: "json",
 		url: "js/data.json",
 		jsonp: "$callback",
-		success: starPage
+		success: feedPage
 	});
-	function starPage( data ) {
+	function feedPage( data ) {
 		$("#photoTemplate").tmpl(data).appendTo(".photos");
 		$("#headerTemplate").tmpl(data).appendTo("header");
 		$("#descriptionTemplate").tmpl(data).appendTo(".description");
 		$("#facilitiesTemplate").tmpl(data).appendTo("#facilities");
 		$("#roomsTemplate").tmpl(data).appendTo("tbody");
+		$("#reviewsTemplate").tmpl(data).appendTo(".reviews_list");
+
+		pagination();
 	}
 
 
+
+
+	////////////////////////////////////////////////////////////
+	// CALCULATE THE TOTAL PRICE
+	///////////////////////////////////////////////////////////
 
 	$(document).on('change', '.room_quantity select', function() {
 		var cost = $(this).attr('data-price');
@@ -167,8 +167,8 @@
 			var qnt = $(this).val()
 			var price = $(this).attr( 'data-price' );
 			var sum = parseFloat(price.replace(',','.')) * parseFloat(qnt);
-	        t_sum += sum;
-	        total_sum = t_sum.toFixed(2);
+			t_sum += sum;
+			total_sum = t_sum.toFixed(2);
 			$("#total_price").text(total_sum);
 		});
 		$(".rooms h3").addClass("alert_1").delay(1000).queue(function(next){
@@ -177,62 +177,56 @@
 		});
 	}
 
-
-
-
 	function priceOrder(){
-
 		orderRoom = [];
-
 		$( ".one_room" ).each(function() {
-
 			var occupancy = $(this).find(".room_occupancy").text();
 			var price = $(this).find("select").attr( 'data-price' );
-				
-			//console.log(occupancy,price);
 			orderRoom.push({occupancy,price});
 
 		});
 
-
 		var test = orderRoom.sort(function(a, b){return a-b});
-		console.log(test);
 	}
 
+	var f_sl = 1;
+	var f_nm = 1;
+	$(document).on('click', '.room_occupancy, .room_price', function(e) {
+		e.preventDefault();
+		f_nm *= -1;
+		var n = $(this).prevAll().length;
+		sortTable(f_nm,n);		
+
+	});
+
+	function sortTable(f,n){
+		var rows = $('.rooms_table tbody  tr').get();
+
+		rows.sort(function(a, b) {
+			var A = $(a).children('td').eq(n).text().toUpperCase();
+			var B = $(b).children('td').eq(n).text().toUpperCase();
+			if(A < B) {
+				return -1*f;
+			}
+			if(A > B) {
+				return 1*f;
+			}
+			return 0;
+		});
+		$.each(rows, function(index, row) {
+			$('.rooms_table').children('tbody').append(row);
+		});
+	};
 
 
 
 
+pagination = function(){
+	var perPage = 5;
+	var total = $(".one_review").size();
 
-
-
-function sortTable(f,n){
-  var rows = $('.rooms_table tbody  tr').get();
-
-  rows.sort(function(a, b) {
-
-  var A = $(a).children('td').eq(n).text().toUpperCase();
-  var B = $(b).children('td').eq(n).text().toUpperCase();
-
-  if(A < B) {
-    return -1*f;
-  }
-  if(A > B) {
-    return 1*f;
-  }
-  return 0;
-  });
-
-  $.each(rows, function(index, row) {
-    $('.rooms_table').children('tbody').append(row);
-  });
-};
-
-
-
-
-
-
+	console.log(total);
+}
 
 
 
